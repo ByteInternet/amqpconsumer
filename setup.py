fa8a2493ced2dffa6e937ebabe842b828c07b34d
Bono de Visser <bono@byte.nl>
2021-05-28 16:07:14 +0200

    Up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='amqpconsumer',
-    version='1.7.2',
+    version='1.7.3',
     description='AMQP event listener',
     url='https://github.com/ByteInternet/amqpconsumer',
     author='Byte B.V.',
